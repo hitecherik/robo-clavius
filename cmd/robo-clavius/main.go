8a362f6c7f373b94017a4210301cd7fdf4396394
Alexander Nielsen <hitecherik@gmail.com>
2020-04-20 23:59:00 +0100

    Add command-line parsing

--- cmd/robo-clavius/main.go
+++ cmd/robo-clavius/main.go
@@ -1,9 +1,39 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"time"
 )
 
+const layout = "2006-01-02"
+
+type targets struct {
+	dates []time.Time
+}
+
+func (t *targets) String() string {
+	return fmt.Sprint(t.dates)
+}
+
+func (t *targets) Set(value string) error {
+	date, err := time.Parse(layout, value)
+
+	if err != nil {
+		return err
+	}
+
+	t.dates = append(t.dates, date)
+	return nil
+}
+
+var dates targets
+
+func init() {
+	flag.Var(&dates, "target", "date the money needs to be out of the pot on")
+}
+
 func main() {
-	fmt.Printf("Hello, world!")
+	flag.Parse()
+	fmt.Println(dates.String())
 }
